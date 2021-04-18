aa2d717a772e528de3809f028e5f2fa600c00252
Viktor Casubolo <viktor.casubolo98@gmail.com>
2021-04-18 15:36:31 +0200

    Update VideoPlayer-trailers

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,12 +13,22 @@ var app = new Vue({
         inputSearch: false,
         showMoviesOrTV: true,
         mute: 'mute=1',
-        keys: ['6ZfuNTqbHE8', 'v7X03INFQRg', 'ybji16u608U', '3cxixDgHUYw'],
-        keysTV: ['IWBsDaFWyTE', 'nW948Va-l10', '3mTh2joPPj8', '2yrviapP5uY'],
+        keys: [
+            'https://ia802806.us.archive.org/10/items/marvelstudiosavengersendgameofficialtrailer/Marvel%20Studios%27%20Avengers-%20Endgame%20-%20Official%20Trailer.mp4',
+            'https://ia903003.us.archive.org/23/items/podcast_marvel-deutschland_captain-america-the-first-ave_1000168356931/podcast_marvel-deutschland_captain-america-the-first-ave_1000168356931.m4v',
+            'https://ia802908.us.archive.org/22/items/trailer1_201701/trailer1.mp4',
+            'https://ia801002.us.archive.org/32/items/podcast_marvel-deutschland_thor-trailer-2011-ipodi_1000168356941/podcast_marvel-deutschland_thor-trailer-2011-ipodi_1000168356941.m4v'
+        ],
+        keysTV: ['https://ia800909.us.archive.org/28/items/4LuciferSeason4TeaserHDNetflixYouTube/%284%29%20Lucifer%20-%20Season%204%20Teaser%20%5BHD%5D%20-%20Netflix%20-%20YouTube.mp4',
+        'https://ia601807.us.archive.org/35/items/wanda-vision-official-trailer-disney/WandaVision%20_%20Official%20Trailer%20_%20Disney%2B.mp4',
+        'https://ia601205.us.archive.org/35/items/Httparrowseason5fullepisodes.com/Arrow%20Season%205%20Trailer%20%28Comic-Con%29.mp4',
+        'https://ia601807.us.archive.org/35/items/wanda-vision-official-trailer-disney/WandaVision%20_%20Official%20Trailer%20_%20Disney%2B.mp4'
+        ],
         indexVideo: '',
         colorBind: '',
         colorBindTv: '',
-        iframe: '#iframe',
+        display: false,
+
     },
     mounted(){
         this.indexVideo= Math.floor(Math.random() * (this.keys.length - 0)) + 0
